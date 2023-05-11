77a879f8a103bf129e697dab30c57d963d2ae9c1
YashRaj5 <YashRaj5>
2023-05-11 07:48:16 +0000

    done

--- resources_used.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC * [Real-Time Data Streaming With Databricks, Spark & Power BI by Insight](https://www.insight.com/en_US/content-and-resources/tech-tutorials/real-time-data-streaming-with-databricks-spark-and-power-bi.html)
-# MAGIC * 
